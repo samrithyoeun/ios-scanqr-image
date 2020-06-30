5fdfbf630bf76aba2a2c9ce58df3df6bc8fc6f7c
sam yoeun <sam.yoeun@pathmazing.com>
2020-06-30 20:03:13 +0700

    Initial Commit

---
+++ ios-scanqr-image/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-scanqr-image
+//
+//  Created by Samrith Yoeun on 6/30/20.
+//  Copyright © 2020 Sammi Yoeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
